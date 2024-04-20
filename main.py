c5113f2f39476356a0b4b7c82e212f3b7b66b89d
Vitali Baranov <baragvit@yandex.ru>
2024-04-20 07:07:25 +0300

    skip download for request with redirect

--- main.py
+++ main.py
@@ -3,17 +3,29 @@ import os
 import requests
 
 
-def download_books():
-    os.makedirs('books', exist_ok=True)
-    for i in range(1, 11):
-        response = requests.get('https://tululu.org/txt.php', params={"id": i})
-        response.raise_for_status()
-        with open(f'books/id{i}.txt', 'w') as book:
-            book.write(response.text)
+def check_for_redirect(response):
+    for history_response in response.history:
+        if history_response.is_redirect:
+            raise requests.exceptions.HTTPError()
+
+
+def download_book(book_id):
+    response = requests.get("https://tululu.org/txt.php", params={"id": book_id})
+    response.raise_for_status()
+    check_for_redirect(response)
+    return response.text
+
+
+def download_books(books_download_directory, book_start_id, book_end_id):
+    for book_id in range(book_start_id, book_end_id + 1):
+        try:
+            book_text = download_book(book_id)
+        except requests.exceptions.HTTPError:
+            continue
+        with open(f'{books_download_directory}/id{book_id}.txt', 'w') as book_file:
+            book_file.write(book_text)
 
 
 if __name__ == '__main__':
-    download_books()
-
-
-# https://tululu.org/txt.php?id=32168
+    os.makedirs('books', exist_ok=True)
+    download_books('books', 1, 10)
