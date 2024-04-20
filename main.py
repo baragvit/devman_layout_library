89ee829a8f86a4fd4e170e97fce009d7043927d8
Vitali Baranov <baragvit@yandex.ru>
2024-04-20 17:19:02 +0300

    download book with title

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
+import logging
 
 import requests
 from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
+logger = logging.getLogger(__name__)
 
 
 def check_for_redirect(response):
@@ -10,35 +13,45 @@ def check_for_redirect(response):
             raise requests.exceptions.HTTPError()
 
 
-def download_book(book_id):
-    response = requests.get("https://tululu.org/txt.php", params={"id": book_id})
+def get_book_author_and_title(book_id):
+    url = f'https://tululu.org/b{book_id}/'
+    response = requests.get(url)
     response.raise_for_status()
     check_for_redirect(response)
-    return response.text
+    soup = BeautifulSoup(response.text, "lxml")
+    title_tag = soup.find('h1')
+    return (title.strip() for title in title_tag.text.split('::'))
+
+
+def download_txt(book_url, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    response = requests.get(book_url)
+    response.raise_for_status()
+    check_for_redirect(response)
+    filename = sanitize_filename(filename)
+    book_file_path = os.path.join(folder, filename + '.txt')
+    with open(book_file_path, 'w') as book_file:
+        book_file.write(response.text)
 
 
 def download_books(books_download_directory, book_start_id, book_end_id):
     for book_id in range(book_start_id, book_end_id + 1):
         try:
-            book_text = download_book(book_id)
+            book_title, _ = get_book_author_and_title(book_id)
+            book_url = f"https://tululu.org/txt.php?id={book_id}"
+            download_txt(book_url, f"{book_id}. {book_title}", books_download_directory)
         except requests.exceptions.HTTPError:
+            logger.warning(f"Failed to download book with id={book_id}")
             continue
-        with open(f'{books_download_directory}/id{book_id}.txt', 'w') as book_file:
-            book_file.write(book_text)
-
-
-def print_book_title(book_id):
-    url = f'https://tululu.org/b{book_id}/'
-    response = requests.get(url)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, "lxml")
-    title_tag = soup.find('h1')
-    name, author = [title.strip() for title in title_tag.text.split('::')]
-    print(f"Заголовок: {name}")
-    print(f"Автор: {author}")
 
 
 if __name__ == '__main__':
-    # os.makedirs('books', exist_ok=True)
-    # download_books('books', 1, 10)
-    print_book_title(1)
+    os.makedirs('books', exist_ok=True)
+    download_books('books', 1, 10)
