9b32d71ae83dbf80333111470adbcbd1a173c051
Vitali Baranov <baragvit@yandex.ru>
2024-04-20 06:48:04 +0300

    download books from tululu.org

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+import os
+
 import requests
-def download_book():
-    response = requests.get('https://tululu.org/txt.php?id=32168')
-    response.raise_for_status()
-    with open('book.txt', 'w') as b:
-        b.write(response.text)
+
+
+def download_books():
+    os.makedirs('books', exist_ok=True)
+    for i in range(1, 11):
+        response = requests.get('https://tululu.org/txt.php', params={"id": i})
+        response.raise_for_status()
+        with open(f'books/id{i}.txt', 'w') as book:
+            book.write(response.text)
 
 
 if __name__ == '__main__':
-    download_book()
+    download_books()
+
+
+# https://tululu.org/txt.php?id=32168
